1340d66621ecf3fd85cdf86556e550be22540172
biel-igl <gabrielpires524@gmail.com>
2023-09-16 13:17:30 -0300

    cria a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,22 @@
-# Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news_data = find_news()
+
+    categories_counter = {}
+
+    for news in news_data:
+        category = news["category"]
+        if category in categories_counter:
+            categories_counter[category] += 1
+        else:
+            categories_counter[category] = 1
+
+    sorted_categories = sorted(
+        categories_counter.items(), key=lambda x: (-x[1], x[0])
+    )
+
+    top_5 = [category for category, count in sorted_categories[:5]]
+
+    return top_5
