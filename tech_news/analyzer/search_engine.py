e38ce322d9766a4521560becab5e00c139d8f20d
biel-igl <gabrielpires524@gmail.com>
2023-09-16 13:05:42 -0300

    cria a função search_by_title e a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,29 @@
-# Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+from datetime import datetime
+from tech_news.database import db
 
 
-# Requisito 8
-def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+def search_by_title(title: str):
+    lower_title = title.lower()
+    result = db.news.find(
+        {"title": {"$regex": f".*{lower_title}.*", "$options": "i"}},
+        {"title": 1, "url": 1},
+    )
+
+    return [(news["title"], news["url"]) for news in result]
+
+
+def search_by_date(date_str: str):
+    try:
+        search_date = datetime.strptime(date_str, "%Y-%m-%d")
+        formatted_date = search_date.strftime("%d/%m/%Y")
+
+        result = db.news.find(
+            {"timestamp": formatted_date}, {"title": 1, "url": 1}
+        )
+
+        return [(news["title"], news["url"]) for news in result]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
